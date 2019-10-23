6cc4b74a7e9597bd6aaee51f03e599d728f02a3a
Kristjan Mar Kjartansson <krissikja@gmail.com>
2019-10-23 22:05:07 +0000

    Vantar coments

--- catalog.py
+++ catalog.py
@@ -1,8 +1,8 @@
 class Item():
 
-    def __init__(self, name = '', category = ''):
-        self.name = name
-        self.category = category
+    def __init__(self, __name = '', __category = ''):
+        self.name = __name
+        self.category = __category
 
     def set_name(self, name = ''):
         self.name = name
@@ -15,12 +15,12 @@ class Item():
 
 class Catalog():
 
-    def __init__(self, name = ''):
-        self.name = name
+    def __init__(self, __name = ''):
+        self.name = __name
         self.list = list()
 
     def set_name(self, name = ''):
-        self.name = name
+        self.__name = name
 
     def add(self, item = ''):
         self.list.append(item.__str__())
@@ -40,41 +40,7 @@ class Catalog():
 
     def __str__(self):
         if len(self.list) == 0:
-            STR = 'Catalog {}: '.format(self.name)
+            STR = 'Catalog {}:'.format(self.name)
         else:
-            STR = 'Catalog {}: '.format(self.name) + '\n\t'+ '\n\t'.join(self.list)
+            STR = 'Catalog {}:'.format(self.name) + '\n\t' + '\n\t'.join(self.list)
         return STR
-
-# ---Main Program ---
-
-item1 = Item("Green Book", "Biography")
-print(item1)
-
-item2 = Item("The God", "Crime")
-print(item2)
-
-item2.set_name("The Godfather")
-print(item2)
-
-item3 = Item("Schindler's List", "Biography")
-print(item3)
-item3.set_category("Drama")
-print(item3)
-
-catalog = Catalog('Films')
-catalog.add(item1)
-catalog.add(item2)
-catalog.add(item3)
-print(catalog)
-
-catalog.remove(item2)
-print(catalog)
-
-catalog.set_name("Favorite Movies")
-print(catalog)
-
-print(catalog.find_item_by_name("Green Book"))
-print(catalog.find_item_by_name("The Godfather"))
-
-catalog.clear()
-print(catalog)
